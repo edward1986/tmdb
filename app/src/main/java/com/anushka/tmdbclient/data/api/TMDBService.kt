0fbb5e81df45d8a4c32838828fe44d94ebda3e38
Lorilla Family <edwardlorilla1986@gmail.com>
2021-07-07 23:43:24 +0800

    first commit

--- app/src/main/java/com/anushka/tmdbclient/data/api/TMDBService.kt
+++ app/src/main/java/com/anushka/tmdbclient/data/api/TMDBService.kt
@@ -1,9 +1,9 @@
 package com.anushka.tmdbclient.data.api
 
 
-import com.anushka.tmdbclient.data.ArtistList
-import com.anushka.tmdbclient.data.MovieList
-import com.anushka.tmdbclient.data.TvShowList
+import com.anushka.tmdbclient.data.model.artist.ArtistList
+import com.anushka.tmdbclient.data.model.movie.MovieList
+import com.anushka.tmdbclient.data.model.tvshow.TvShowList
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
