55cfc2ecbca3dc102ea9e72b94e6910283fb690c
Lorilla Family <edwardlorilla1986@gmail.com>
2021-07-16 00:21:04 +0800

    first commit

--- app/src/main/java/com/anushka/tmdbclient/presentation/movie/MovieActivity.kt
+++ app/src/main/java/com/anushka/tmdbclient/presentation/movie/MovieActivity.kt
@@ -4,6 +4,9 @@ import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
@@ -54,5 +57,40 @@ class MovieActivity : AppCompatActivity() {
             }
         })
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater : MenuInflater = menuInflater
+        inflater.inflate(R.menu.update,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId){
+            R.id.action_update -> {
+                updateMovies()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+
+    }
+
+    private fun updateMovies(){
+      binding.movieProgressBar.visibility = View.VISIBLE
+      val response = movieViewModel.updateMovies()
+      response.observe(this, Observer {
+        if(it!=null){
+          adapter.setList(it)
+          adapter.notifyDataSetChanged()
+          binding.movieProgressBar.visibility = View.GONE
+        }else{
+          binding.movieProgressBar.visibility = View.GONE
+        }
+      })
+    }
+
+
+
+
 
 }
